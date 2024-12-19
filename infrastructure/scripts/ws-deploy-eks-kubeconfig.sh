5e75d47d3838199e670fb1be871254cbd9a11b7e
Yuriy Bezsonov <bezsonov@amazon.de>
2024-12-19 12:25:13 +0100

    refactor

--- infrastructure/scripts/ws-deploy-eks-kubeconfig.sh
+++ infrastructure/scripts/ws-deploy-eks-kubeconfig.sh
@@ -1,34 +1,6 @@
 set -e
 
-STACK_NAME="WorkshopEksStack"
-CLUSTER_NAME=unicorn-store
-
-check_stack() {
-    STATUS=$(aws cloudformation describe-stacks \
-        --stack-name "$STACK_NAME" \
-        --region "$AWS_REGION" \
-        --query "Stacks[0].StackStatus" \
-        --output text 2>/dev/null)
-
-    case $STATUS in
-        CREATE_COMPLETE|UPDATE_COMPLETE)
-            echo "Stack $STACK_NAME is ready."
-            return 0
-            ;;
-        CREATE_IN_PROGRESS|UPDATE_IN_PROGRESS|UPDATE_COMPLETE_CLEANUP_IN_PROGRESS)
-            echo "Stack $STACK_NAME is still in progress. Current status: $STATUS. Retrying in 10 seconds..."
-            return 1
-            ;;
-        *)
-            if [ -z "$STATUS" ]; then
-                echo "Stack $STACK_NAME does not exist or an error occurred while checking. Retrying in 10 seconds..."
-            else
-                echo "Stack $STACK_NAME is in an unexpected state: $STATUS". Retrying in 10 seconds...
-            fi
-            return 1
-            ;;
-    esac
-}
+CLUSTER_NAME=${1:-"unicorn-store"}
 
 check_cluster() {
     cluster_status=$(aws eks describe-cluster --name "$CLUSTER_NAME" --query "cluster.status" --output text 2>/dev/null)
@@ -39,8 +11,6 @@ check_cluster() {
     echo "EKS cluster is active."
     return 0
 }
-
-while ! check_stack; do sleep 10; done
 
 while ! check_cluster; do sleep 10; done
 
